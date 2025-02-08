4113e4c59d088de66c52dd1b2bd743e43cccd458
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-08 13:56:06 +0100

    Understanding Arrays

--- Program.cs
+++ Program.cs
@@ -1,24 +1,46 @@
 ﻿using System;
 
-namespace ForIteration
+namespace UnderstandingArrays
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i);
-                if (i == 7)
-                {
-                    Console.WriteLine("Found seven!");
-                    break;
-                }
-            }
+            //int[] numbers = new int[5];
 
-            for (int myValue = 0; myValue < 12; myValue++)
-            {
-                Console.WriteLine(myValue);
+            //numbers[0] = 4;
+            //numbers[1] = 8;
+            //numbers[2] = 15;
+            //numbers[3] = 16;
+            //numbers[4] = 23;
+            ////numbers[5] = 42;
+
+            //Console.WriteLine(numbers);
+            //Console.WriteLine(numbers[1]);
+            //Console.WriteLine(numbers.Length);
+
+            int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
+            string[] names = new string[] { "Eddie", "Alex", "Michael", "David Lee" };
+
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine(names[i]);
+            //}
+
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            string zig = 
+                "You can get what you want out of life " +
+                "if you help enough other people get what they want.";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray) {
+                Console.Write(zigChar);
             }
 
             Console.ReadLine();
