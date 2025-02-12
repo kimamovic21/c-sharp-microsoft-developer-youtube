2454f3a26ec73b91bd451bfe67205d63b8972f44
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-12 14:05:08 +0100

    Working with Dates and Times

--- Program.cs
+++ Program.cs
@@ -1,49 +1,31 @@
 ﻿using System;
-using System.Text;
 
-namespace WorkingWithStrings
+namespace DatesAndTimes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //string myString = "My \"so-called\" life";
-            //string myString = "What if I need a \nnew line?";
-            //string myString = "Go to your c:\\ drive";
-            //string myString = @"Go to your c:\ drive";
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
 
-            //string myString = String.Format("{0} = {1}", "First", "Second");
-            //string myString = string.Format("{0:C}", 123.45);
-            //string myString = string.Format("{0:N}", 1234567890);
-            //string myString = string.Format("{0:P}", .123);
-            //string myString = string.Format("Phone Number: {0:(###) ###-####}", 1234567890);
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
 
-            //string myString = " That summer we took threes across the board ";
-            //myString = myString.Substring(6, 14);
-            //myString = myString.ToUpper();
-            //myString = myString.Replace(" ", "--");
-            //myString = myString.Remove(6, 14);
-            //myString = String.Format(
-            //    "Length before: {0} -- Length after: {1}", 
-            //    myString.Length, 
-            //    myString.Trim().Length
-            //);
+            //Console.WriteLine(myValue.Month);
 
-            //string myString = "";
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    myString += "--" + i.ToString();
-            //}
+            //DateTime myBirthday = new DateTime(1996, 11, 28);
+            //Console.WriteLine(myBirthday.ToShortDateString());
 
-            StringBuilder myString = new StringBuilder();
+            DateTime myBirthday = DateTime.Parse("11/28/1996");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
 
-            for (int i = 0; i < 100; i++)
-            {
-                myString.Append("--");
-                myString.Append(i);
-            }
-
-            Console.WriteLine(myString);
             Console.ReadLine();
         }
     }
