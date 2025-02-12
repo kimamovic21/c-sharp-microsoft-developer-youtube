d447d6664a4753734a4dd67dd252a0bc6a6c95f0
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-12 10:56:33 +0100

    While Iteration statement

--- Program.cs
+++ Program.cs
@@ -1,43 +1,86 @@
 ﻿using System;
 
-namespace SimpleMethods
+namespace WhileIteration
 {
     class Program
     {
         static void Main(string[] args)
         {
-            HelloWorld();
+            bool displayMenu = true;
 
-            Console.WriteLine("What is your first name?");
-            string firstName = Console.ReadLine();
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
 
-            Console.WriteLine("What is your last name?");
-            string lastName = Console.ReadLine();
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Print Numbers");
+            Console.WriteLine("2) Guessing Game");
+            Console.WriteLine("3) Exit");
 
-            string reversedFirstName = ReverseString(firstName);
-            string reversedLastName = ReverseString(lastName);
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                PrintNumbers();
+                return true;
+            }
+            else if (result == "2")
+            {
+                GuessingGame();
+                return true;
+            }
+            else if (result == "3")
+                return false;
+            else
+                return true;
+        }
 
-            DisplayResult(reversedFirstName, reversedLastName);
-
+        private static void PrintNumbers()
+        {
+            Console.Clear();
+            Console.WriteLine("Print numbers");
+            Console.WriteLine("Type a number:");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter < result + 1)
+            {
+                Console.Write(counter);
+                Console.WriteLine("-");
+                counter++;
+            }
             Console.ReadLine();
         }
 
-        private static void HelloWorld()
+        private static void GuessingGame()
         {
-            Console.WriteLine("Hello World");
-        }
+            Console.Clear();
+            Console.WriteLine("Guessing game");
 
-        private static string ReverseString(string message)
-        {
-            char[] messageArray = message.ToCharArray();
-            Array.Reverse(messageArray);
+            Random myRandom = new Random();
+            int randomNumber = myRandom.Next(1, 11);
 
-            return new string(messageArray);
-        }
+            int guesses = 0;
+            bool incorrect = true;
 
-        private static void DisplayResult(string reversedFirstName, string reversedLastName)
-        {
-            Console.WriteLine($"Reversed Name: {reversedFirstName} {reversedLastName}");
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10");
+                string result = Console.ReadLine();
+
+                guesses++;
+
+                if (result == randomNumber.ToString())
+                    incorrect = false;
+                else
+                    Console.WriteLine("Wrong!");
+            } while(incorrect);
+            Console.WriteLine("Correct! It took you {0} guesses.", guesses);
+
+            Console.ReadLine();
         }
     }
 }
