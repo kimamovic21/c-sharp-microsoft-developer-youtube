223e21cbf1e938a6a79b6ccdffeaddadebb7bcc1
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-14 17:54:27 +0100

    Creating and Adding References to Assemblies

--- Program.cs
+++ Program.cs
@@ -1,20 +1,39 @@
 ﻿using System;
-using System.IO;
+using System.IO; 
 using System.Net;
 
-namespace AssembliesAndNamespaces
+namespace CreatingAndAddingReferences
 {
     class Program
     {
         static void Main(string[] args)
         {
-            WebClient client = new WebClient();
-            string reply = client.DownloadString("https://msdn.microsoft.com");
-
-            Console.WriteLine(reply);
-            File.WriteAllText(@"C:\Lesson17\WriteText.txt", reply);
-
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
             Console.ReadLine();
+        }
+
+
+        public class Scrape
+        {
+            public string ScrapeWebpage(string url)
+            {
+                return GetWebpage(url);
+            }
+
+            public string ScrapeWebpage(string url, string filepath)
+            {
+                string reply = GetWebpage(url);
+                File.WriteAllText(filepath, reply);
+                return reply;
+            }
+
+            private string GetWebpage(string url)
+            {
+                WebClient client = new WebClient();
+                return client.DownloadString(url);
+            }
         }
     }
 }
