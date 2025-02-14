1fe6f8bfb8593841e1dc1fbc75a5317dbe01799c
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-14 13:37:24 +0100

    More About Classes and Methods

--- Program.cs
+++ Program.cs
@@ -1,37 +1,49 @@
 ﻿using System;
 
-namespace SimpleClasses
+namespace ObjectLifetime
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Car myCar = new Car();
-            myCar.Make = "Chevrolet";
-            myCar.Model = "Camaro";
-            myCar.Year = 2018;
-            myCar.Color = "Red";
+            //Car myCar = new Car();
+            //myCar.Make = "Chevrolet";
+            //myCar.Model = "Camaro";
+            //myCar.Year = 2018;
+            //myCar.Color = "Red";
+            //Console.WriteLine(myCar);
 
-            Console.WriteLine(
-                "{0} {1} {2} {3}", 
-                myCar.Make, 
-                myCar.Model, 
-                myCar.Year, 
-                myCar.Color
-            );
+            //Car myOtherCar;
+            //myOtherCar = myCar;
 
-            //decimal value = DetermineMarketValue(myCar);
-            //Console.WriteLine("{0:C}", value);
-            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+            //Console.WriteLine(
+            //    "{0} {1} {2} {3}",
+            //    myOtherCar.Make,
+            //    myOtherCar.Model,
+            //    myOtherCar.Year,
+            //    myOtherCar.Color
+            //);
+
+            //myOtherCar.Make = "Chevy";
+            //myOtherCar.Model = "Corvette";
+            //myOtherCar.Year = 2019;
+            //myOtherCar.Color = "Blue";
+
+            //myOtherCar = null;
+
+            //Console.WriteLine(
+            //   "{0} {1} {2} {3}",
+            //   myOtherCar.Make,
+            //   myOtherCar.Model,
+            //   myOtherCar.Year,
+            //   myOtherCar.Color
+            //);
+
+            Car myThirdCar = new Car("Ford", "Escape", 2005, "white");
+            Console.Write(myThirdCar);
 
             Console.ReadLine();
         }
-
-        //private static decimal DetermineMarketValue(Car car)
-        //{
-        //    decimal carValue = 14000.0M;
-        //    return carValue;
-        //}
     }
 
     class Car
@@ -41,14 +53,25 @@ namespace SimpleClasses
         public int Year { get; set; }
         public string Color { get; set; }
 
-        public decimal DetermineMarketValue()
+        //public Car()
+        //{
+        //    Make = "Nissan";
+        //    Model = "Altima";
+        //    Year = 2018;
+        //    Color = "Black";
+        //}
+
+        public Car(string make, string model, int year, string color)
         {
-            decimal carValue;
-            if (Year > 2000)
-                carValue = 14000;
-            else
-                carValue = 5000;
-            return carValue;
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"{Year} {Make} {Model} {Color}";
         }
     }
 }
