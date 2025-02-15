4e06f75e014e8de5b58d03830658cc30de0911f4
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-15 15:16:13 +0100

    Working with LINQ

--- Program.cs
+++ Program.cs
@@ -3,83 +3,94 @@ using System.IO;
 using System.Net;
 using System.Collections;
 
-namespace WorkingWithCollections
+namespace WorkingWithLINQ
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Car car1 = new Car();
-            car1.Make = "Toyota";
-            car1.Model = "Camry";
-            car1.VIN = "A1";
-
-            Car car2 = new Car();
-            car2.Make = "Nissan";
-            car2.Model = "Altima";
-            car2.VIN = "B2";
-
-            Book book1 = new Book();
-            book1.Title = "Microsoft .NET XML Web Services";
-            book1.Author = "Bob Tabor";
-            book1.ISBN = "0-000-00000-0";
-
-            // ArrayLists are dynamically sized
-            // cool features sorting, remove items
-            ArrayList myArrayList = new ArrayList();
-            myArrayList.Add(car1);
-            myArrayList.Add(car2);
-            myArrayList.Add(book1);
-            myArrayList.Remove(book1);
-
-            foreach (Car car in myArrayList)
+            List<Car> myCars = new List<Car>()
             {
-                Console.WriteLine(car.Make);
-            }
-
-            // List<T> are type safe
-            List<Car> myList = new List<Car>();
-            myList.Add(car1);
-            myList.Add(car2);
-            //myList.Add(book1); // This would generate a compile error
-
-            foreach (Car car in myList)
-            {
-                Console.WriteLine(car.Model);
-            }
-
-            // Dictionary<TKey, TValue>
-            Dictionary<string, Car> myDictionary = new Dictionary<string, Car>();
-            
-            myDictionary.Add(car1.VIN, car1);
-            myDictionary.Add(car2.VIN, car2);
-
-            Console.WriteLine(myDictionary["B2"].Make);
-
-
-            string[] names = { "Bob", "Steve", "Brian", "Chuck" };
-
-            foreach (string name in names)
-            {
-                Console.WriteLine(name);
-            }
-
-            // Objetct initializer
-            // No need for a constructor
-            Car car3 = new Car() { Make = "BMW", Model = "750li", VIN="C3"};
-            Car car4 = new Car() { Make = "Toyota", Model = "4Runner", VIN = "D4" };
-
-            // Collection initializer
-            List<Car> myList2 = new List<Car>()
-            {
-                new Car { Make = "Oldsmobile", Model = "Cutlas Supreme", VIN = "E5" },
-                new Car { Make = "Chevrolet", Model = "Impala", VIN = "F6" }
+                new Car { Make = "Toyota", Model = "Camry", VIN = "A1", StickerPrice = 25000, Year = 1982 },  
+                new Car { Make = "Nissan", Model = "Altima", VIN = "B2", StickerPrice = 22000, Year = 1993 }, 
+                new Car { Make = "BMW", Model = "750li", VIN = "C3", StickerPrice = 70000, Year = 1987 },
+                new Car { Make = "BMW", Model = "55i", VIN = "D4", StickerPrice = 55000, Year = 2004 },  
+                new Car { Make = "Toyota", Model = "4Runner", VIN = "E5", StickerPrice = 35000, Year = 1984 },
+                new Car { Make = "Ford", Model = "Escape", VIN = "F6", StickerPrice = 30000, Year = 2000 },
             };
 
-            foreach (Car car in myList2)
-            {
-                Console.WriteLine(car.Model);
-            }
+            //var bmws = from car in myCars
+            //           where car.Make == "BMW"
+            //           && car.Year == 2004
+            //           select car;
+
+            //foreach (var car in bmws)
+            //{
+            //    Console.WriteLine("{0} {1}", car.Model, car.VIN);
+            //}
+
+
+            //var bmws2 = myCars.Where(p => p.Make == "BMW" && p.Year == 2004);
+
+            //foreach (var car in bmws2)
+            //{
+            //    Console.WriteLine("{0} {1}", car.Model, car.VIN);
+            //}
+
+
+            //var orderedCars = from car in myCars
+            //                  orderby car.Year descending
+            //                  select car;
+
+            //foreach (var car in orderedCars)
+            //{
+            //    Console.WriteLine("{0} {1}", car.Year, car.Model, car.VIN);
+            //}
+
+
+            //var orderedCars = myCars.OrderByDescending(p => p.Year);
+
+            //foreach (var car in orderedCars)
+            //{
+            //    Console.WriteLine("{0} {1}", car.Year, car.Model, car.VIN);
+            //}
+
+
+            //var firstBMW = myCars
+            //    .OrderByDescending(p => p.Year)
+            //    .First(p => p.Make == "BMW");
+            //Console.WriteLine(firstBMW.VIN);
+
+
+            //Console.WriteLine(myCars.TrueForAll(p => p.Year > 2000));
+
+
+            //myCars.ForEach(p => p.StickerPrice -= 3000);
+            //myCars.ForEach(p => Console.WriteLine("{0} {1:C}", p.VIN, p.StickerPrice));
+
+
+            //Console.WriteLine(myCars.Exists(p => p.Model == "750li"));
+
+
+            //Console.WriteLine(myCars.Sum(p => p.StickerPrice));
+
+
+            //Console.WriteLine(myCars.GetType());
+
+
+            //var orderedCars = myCars.OrderByDescending(p => p.Year);
+            //Console.WriteLine(orderedCars.GetType());
+
+
+            //var bmws = myCars.Where(p => p.Make == "BMW" && p.Year == 2004);
+            //Console.WriteLine(bmws.GetType());
+
+
+            //var newCars = from car in myCars
+            //              where car.Make == "BMW"
+            //              && car.Year == 2004
+            //              select new { car.Make, car.Model };
+            //Console.WriteLine(newCars.GetType());
 
             Console.ReadLine();
         }
@@ -90,12 +101,7 @@ namespace WorkingWithCollections
         public string VIN { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
-    }
-
-    class Book
-    {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string ISBN { get; set; }
+        public int Year { get; set; }
+        public double StickerPrice { get; set; }
     }
 }
