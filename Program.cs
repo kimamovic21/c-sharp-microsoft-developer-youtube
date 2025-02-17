32a10ba1526c92bd76067127856e2b8772862391
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-17 14:36:00 +0100

    Where to Go from here

--- Program.cs
+++ Program.cs
@@ -4,37 +4,14 @@ using System.Net;
 using System.Collections;
 using System.Timers;
 
-namespace TimerExample
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Timers.Timer myTimer = new System.Timers.Timer(1000);
-
-            myTimer.Elapsed += MyTimer_Elapsed;
-            myTimer.Elapsed += MyTimer_Elapsed1;
-
-            myTimer.Start();
-
-            Console.WriteLine("Press enter to remove the red event");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
-
-            myTimer.Elapsed -= MyTimer_Elapsed1;
-
-            Console.ReadLine();
-        }
-
-        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
-        }
-
-        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
         }
     }
 }
